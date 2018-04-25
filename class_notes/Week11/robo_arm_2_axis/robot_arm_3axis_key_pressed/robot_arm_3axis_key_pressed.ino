770979dec4f9029b1063f661a97ebfd10c954d1c
sabrinaramos103 <31417643+sabrinaramos103@users.noreply.github.com>
2018-04-25 11:28:16 -0400

    Add files via upload

---
+++ class_notes/Week11/robo_arm_2_axis/robot_arm_3axis_key_pressed/robot_arm_3axis_key_pressed.ino
@@ -0,0 +1,58 @@
+#include <Servo.h>
+
+Servo servoX;  // create servo object to control an X axis servo
+Servo servoY;  // and for Y
+Servo servoZ;
+
+// (a maximum of eight servo objects can be created!)
+
+int servoXPin = 9;  // what pins are our servos connected to?
+int servoYPin = 10;
+int servoZPin = 11;
+
+int posX = 0;    // variable to store the servo position
+int posY = 0;
+int posZ = 0; // variable to store the other servo position
+
+void setup() {
+  Serial.begin(9600);  // we're gonna use serial comm.
+
+  // "attach" servos
+  servoX.attach(servoXPin);
+  servoY.attach(servoYPin);
+  servoZ.attach(servoZPin);
+}
+
+void loop() {
+  // see if there is available serial data
+  if (Serial.available() > 0) {
+    int inByte = Serial.read();
+
+    if (inByte == 1) {
+      servoX.write(90);
+      servoY.write(50);
+      servoZ.write(30);
+
+      delay(500);
+
+      servoX.write(180);
+      servoY.write(26);
+      servoZ.write(18);
+
+    }
+    if (inByte == 2) {
+      servoX.write(180);
+      servoY.write(180);
+      servoZ.write(180);
+
+    }
+
+    // read the incoming bytes
+    //posX = Serial.read();
+    //posY = Serial.read();
+    //posZ = Serial.read();
+  }
+
+  // "write" for a servo motor just tells it what angle to go to
+
+}
